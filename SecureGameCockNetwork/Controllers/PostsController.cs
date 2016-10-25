8af17ba06886a1b0364bb030e2d853c4d910671d
Yugendhar <yogi.yugendhar@gmail.com>
2016-10-25 04:05:30 -0400

    Added Post Controller to Add comments or posts Installed Entity Framework Package

---
+++ SecureGameCockNetwork/Controllers/PostsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecureGameCockNetwork.Controllers
+{
+    public class PostsController : Controller
+    {
+        private MessageBoardContext _ctx;
+        public PostsController()
+        {
+            this._ctx = new MessageBoardContext();
+        }
+
+        // GET api/<controller>
+        public IEnumerable<Post> Get()
+        {
+            var allPosts = this._ctx.Posts.OrderByDescending(x => x.DatePosted).ToList();
+            
+            //foreach (var post in allPosts)
+            //{
+            //    var allCommentsOnThisPost = _ctx.Comments.Where(p => p.ParentPost.Id == post.Id).ToList();
+            //    foreach (var comment in allCommentsOnThisPost)
+            //    {
+            //        post.Comments.Add(comment);
+            //    }
+            //}
+            return allPosts;
+        }
+    }
+
+    }
+}
---
+++ SecureGameCockNetwork/Models/MessagingBoardContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SecureGameCockNetwork.Models
+{
+    public class MessagingBoardContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Username { get; set; }
+        public DateTime DatePosted { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Username { get; set; }
+        public DateTime DatePosted { get; set; }
+        public virtual Post ParentPost { get; set; }
+    }
+}
