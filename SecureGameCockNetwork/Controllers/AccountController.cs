d92934075a592d552e063d77bd407997907ed0bb
Yugendhar <yogi.yugendhar@gmail.com>
2016-10-25 04:16:39 -0400

    Included Account controller

---
+++ SecureGameCockNetwork/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecureGameCockNetwork.Controllers
+{
+    public class AccountController : Controller
+    {
+        //To Do : Update this file accordingly with account related actions.
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
