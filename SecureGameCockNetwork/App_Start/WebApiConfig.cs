1a2cad38fc07d0f0eb209fcb9f5c50ddc3039b19
Yugendhar <yogi.yugendhar@gmail.com>
2016-10-26 14:55:34 -0400

    Updated WebApi Packages Browser Title added Account Controller to Register

--- SecureGameCockNetwork/App_Start/WebApiConfig.cs
+++ SecureGameCockNetwork/App_Start/WebApiConfig.cs
@@ -9,6 +9,7 @@ namespace SecureGameCockNetwork
     {
         public static void Register(HttpConfiguration config)
         {
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
--- SecureGameCockNetwork/Controllers/AccountController.cs
+++ SecureGameCockNetwork/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace SecureGameCockNetwork.Controllers
@@ -16,5 +17,32 @@ namespace SecureGameCockNetwork.Controllers
             return View();
         }
 
+        //ToDo : Registration Process
+
+        // POST api/Account/Register
+        //[AllowAnonymous]
+        //[Route("Register")]
+        //public async Task<IHttpActionResult> Register(RegisterBindingModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    IdentityUser user = new IdentityUser
+        //    {
+        //        UserName = model.UserName
+        //    };
+
+        //    IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+        //    IHttpActionResult errorResult = GetErrorResult(result);
+
+        //    if (errorResult != null)
+        //    {
+        //        return errorResult;
+        //    }
+
+        //    return Ok();
+        //}
     }
 }
