10ea657d3b4d74af6c6519ae9dfdfa7f4e83d795
Yugendhar <yogi.yugendhar@gmail.com>
2016-10-26 16:48:03 -0400

    Added Register Api to perform registration

--- SecureGameCockNetwork/Controllers/AccountController.cs
+++ SecureGameCockNetwork/Controllers/AccountController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using SecureGameCockNetwork.Models;
 
 namespace SecureGameCockNetwork.Controllers
@@ -18,6 +21,19 @@ namespace SecureGameCockNetwork.Controllers
         //{
         //    return View();
         //}
+        public UserManager<IdentityUser> UserManager { get; private set; }
+        public ISecureDataFormat<AuthenticationTicket> AccessTokenFormat { get; private set; }
+
+        public AccountController()
+        {
+        }
+
+        public AccountController(UserManager<IdentityUser> userManager,
+           ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
+        {
+            UserManager = userManager;
+            AccessTokenFormat = accessTokenFormat;
+        }
 
         //ToDo : Registration Process
 
@@ -26,25 +42,53 @@ namespace SecureGameCockNetwork.Controllers
         [Route("Register")]
         public async Task<IHttpActionResult> Register(RegisterBindingModel model)
         {
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-        //    IdentityUser user = new IdentityUser
-        //    {
-        //        UserName = model.UserName
-        //    };
+            IdentityUser user = new IdentityUser
+            {
+                UserName = model.UserName
+            };
 
-        //    IdentityResult result = await UserManager.CreateAsync(user, model.Password);
-        //    IHttpActionResult errorResult = GetErrorResult(result);
+            IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+            IHttpActionResult errorResult = GetErrorResult(result);
 
-        //    if (errorResult != null)
-        //    {
-        //        return errorResult;
-        //    }
+            if (errorResult != null)
+            {
+                return errorResult;
+            }
 
-           // return Ok();
+            return Ok();
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
             return null;
         }
     }
