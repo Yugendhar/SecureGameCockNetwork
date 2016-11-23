4d39ccc90707ba38c5996ebdf93f0996628080d2
Yugendhar <yogi.yugendhar@gmail.com>
2016-11-22 19:49:47 -0500

    Added Encryption and decryption methods

--- SecureGameCockNetwork/Controllers/PostsController.cs
+++ SecureGameCockNetwork/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web.Http;
 using System.Web.Mvc;
 using SecureGameCockNetwork.Models;
+using SecureGameCockNetwork.Providers;
 
 namespace SecureGameCockNetwork.Controllers
 {
@@ -18,15 +19,17 @@ namespace SecureGameCockNetwork.Controllers
         public IEnumerable<Post> Get()
         {
             var allPosts = this._ctx.Posts.OrderByDescending(x => x.DatePosted).ToList();
-            
-            //foreach (var post in allPosts)
-            //{
-            //    var allCommentsOnThisPost = _ctx.Comments.Where(p => p.ParentPost.Id == post.Id).ToList();
-            //    foreach (var comment in allCommentsOnThisPost)
-            //    {
-            //        post.Comments.Add(comment);
-            //    }
-            //}
+
+            foreach (var post in allPosts)
+            {
+                post.Message = EncryptDecryptProvider.Decrypt(post.Message);
+                var allCommentsOnThisPost = _ctx.Comments.Where(p => p.ParentPost.Id == post.Id).ToList();
+                foreach (var comment in allCommentsOnThisPost)
+                {
+                    comment.Message = EncryptDecryptProvider.Decrypt(comment.Message);
+                    post.Comments.Add(comment);
+                }
+            }
             return allPosts;
         }
     }
--- SecureGameCockNetwork/Hubs/BoardHub.cs
+++ SecureGameCockNetwork/Hubs/BoardHub.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using SecureGameCockNetwork.Models;
+using SecureGameCockNetwork.Providers;
 
 namespace SecureGameCockNetwork.Hubs
 {
@@ -12,11 +13,11 @@ namespace SecureGameCockNetwork.Hubs
         public void WritePost(string username, string message)
         {
             var ctx = new MessagingBoardContext();
-            var post = new Post { Message = message, Username = username, DatePosted = DateTime.Now };
+            var post = new Post { Message = EncryptDecryptProvider.Encrypt(message), Username = username, DatePosted = DateTime.Now };
             ctx.Posts.Add(post);
             ctx.SaveChanges();
 
-            Clients.All.receivedNewPost(post.Id, post.Username, post.Message, post.DatePosted);
+            Clients.All.receivedNewPost(post.Id, post.Username, EncryptDecryptProvider.Decrypt(post.Message), post.DatePosted);
         }
 
         public void AddComment(int postId, string comment, string username)
@@ -26,11 +27,11 @@ namespace SecureGameCockNetwork.Hubs
 
             if (post != null)
             {
-                var newComment = new Comment { ParentPost = post, Message = comment, Username = username, DatePosted = DateTime.Now };
+                var newComment = new Comment { ParentPost = post, Message = EncryptDecryptProvider.Encrypt(comment), Username = username, DatePosted = DateTime.Now };
                 ctx.Comments.Add(newComment);
                 ctx.SaveChanges();
 
-                Clients.All.receivedNewComment(newComment.ParentPost.Id, newComment.Id, newComment.Message, newComment.Username, newComment.DatePosted);
+                Clients.All.receivedNewComment(newComment.ParentPost.Id, newComment.Id, EncryptDecryptProvider.Decrypt(newComment.Message), newComment.Username, newComment.DatePosted);
             }
         }
     }
