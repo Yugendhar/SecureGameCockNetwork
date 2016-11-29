a1f8b8b619b457607339189e4492af6799f60d25
Yugendhar <yogi.yugendhar@gmail.com>
2016-11-29 03:29:07 -0500

    Hash Based Message Authentication Code to Verify Integrity

--- SecureGameCockNetwork/Providers/EncryptDecryptProvider.cs
+++ SecureGameCockNetwork/Providers/EncryptDecryptProvider.cs
@@ -13,6 +13,9 @@ namespace SecureGameCockNetwork.Providers
         private static string Sharedkey = ConfigurationManager.AppSettings["SharedKey"];
 
         private const int Keysize = 256; // can be 192 or 128
+
+        private const int BlockSize = 256; // AES uses only 128 bit block size (FIXED BLOCK SIZE) where as Rinjdael can use 256.
+
         // This constant determines the number of iterations for the password bytes generation function.
         private const int DerivationIterations = 1000;
 
@@ -33,7 +36,7 @@ namespace SecureGameCockNetwork.Providers
             {
                 using (var symmetricKey = new RijndaelManaged())
                 {
-                    symmetricKey.BlockSize = 256;
+                    symmetricKey.BlockSize = BlockSize;
                     symmetricKey.Mode = CipherMode.CBC;
                     symmetricKey.Padding = PaddingMode.PKCS7;
                     // Use the password to generate pseudo - random bytes for the encryption
@@ -52,8 +55,17 @@ namespace SecureGameCockNetwork.Providers
                                 var cipherTextBytes = saltStringBytes;
                                 cipherTextBytes = cipherTextBytes.Concat(initvectorStringBytes).ToArray();
                                 cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+                                
+                                //Assemble encrypted message and add authentication
+                                using (var hmac = new HMACSHA256(keyBytes))
+                                {
+                                    //Authenticate all data
+                                    var tag = hmac.ComputeHash(cipherTextBytes);
+                                    cipherTextBytes = cipherTextBytes.Concat(tag).ToArray();
+                                }
                                 memoryStream.Close();
                                 cryptoStream.Close();
+
                                 return Convert.ToBase64String(cipherTextBytes);
                             }
                         }
@@ -72,37 +84,69 @@ namespace SecureGameCockNetwork.Providers
         {
             // Get the complete stream of bytes that represent:
             // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
-           byte[] cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
+            byte[] cipherTextBytesWithSaltAndIVAndTag = Convert.FromBase64String(cipherText);
 
-           //byte[] cipherTextBytesWithSaltAndIv =Encoding.UTF8.GetBytes(cipherText);
-           // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
-           var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
+            //byte[] cipherTextBytesWithSaltAndIv =Encoding.UTF8.GetBytes(cipherText);
+            // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
+            var saltStringBytes = cipherTextBytesWithSaltAndIVAndTag.Take(Keysize / 8).ToArray();
 
             // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
-            var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
+            var ivStringBytes = cipherTextBytesWithSaltAndIVAndTag.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
 
-            // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
-            var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
+            //Get actual cipher text after substracting tag length and initial (salt and IV) lengths respectively.
 
             using (var password = new Rfc2898DeriveBytes(Sharedkey, saltStringBytes, DerivationIterations))
             {
                 var keyBytes = password.GetBytes(Keysize / 8);
-                using (var symmetricKey = new RijndaelManaged())
+
+
+                //Integrity Verificatoin using MAC Hashing using SHA256
+                using (var hmac = new HMACSHA256(keyBytes))
                 {
-                    symmetricKey.BlockSize = 256;
-                    symmetricKey.Mode = CipherMode.CBC;
-                    symmetricKey.Padding = PaddingMode.PKCS7;
-                    using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                    var sentTag = new byte[hmac.HashSize / 8];
+
+                    // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
+                    var cipherTextBytes = cipherTextBytesWithSaltAndIVAndTag.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIVAndTag.Length - ((Keysize / 8) * 2) - sentTag.Length).ToArray();
+
+                    //Calculate Tag
+                    var calcTag = hmac.ComputeHash(cipherTextBytesWithSaltAndIVAndTag, 0, cipherTextBytesWithSaltAndIVAndTag.Length - sentTag.Length);
+                    var ivLength = (BlockSize / 8);
+
+                    //if message length is to small just return null
+                    if (cipherTextBytesWithSaltAndIVAndTag.Length < sentTag.Length + ivLength)
+                        return null;
+
+                    //Grab Sent Tag and later use this for comparision
+                    Array.Copy(cipherTextBytesWithSaltAndIVAndTag, cipherTextBytesWithSaltAndIVAndTag.Length - sentTag.Length, sentTag, 0, sentTag.Length);
+
+                    //Integrity Verificatoin using MAC Hashing using SHA256, Comparision of HashCode bytes
+                    //Compare Tag with constant time comparison
+                    var compare = 0;
+                    for (var i = 0; i < sentTag.Length; i++)
+                        compare |= sentTag[i] ^ calcTag[i];
+
+                    // if message doesn't authenticate return null
+                    if (compare != 0)
+                        return null;
+
+                    //Symmetric Key Decryption
+                    using (var symmetricKey = new RijndaelManaged())
                     {
-                        using (var memoryStream = new MemoryStream(cipherTextBytes))
+                        symmetricKey.BlockSize = BlockSize;
+                        symmetricKey.Mode = CipherMode.CBC;
+                        symmetricKey.Padding = PaddingMode.PKCS7;
+                        using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
                         {
-                            using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                            using (var memoryStream = new MemoryStream(cipherTextBytes))
                             {
-                                var plainTextBytes = new byte[cipherTextBytes.Length];
-                                var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-                                memoryStream.Close();
-                                cryptoStream.Close();
-                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                                {
+                                    var plainTextBytes = new byte[cipherTextBytes.Length];
+                                    var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                                    memoryStream.Close();
+                                    cryptoStream.Close();
+                                    return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                                }
                             }
                         }
                     }
